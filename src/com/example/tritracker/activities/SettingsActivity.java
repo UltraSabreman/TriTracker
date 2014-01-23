4e8ba20d883e5f41ea9cc7413025a5d645f0d02d
Andrey Byelogurov <byelogurov@gmail.com>
2014-01-22 18:03:55 -0800

    Another working build

--- src/com/example/tritracker/activities/SettingsActivity.java
+++ src/com/example/tritracker/activities/SettingsActivity.java
@@ -123,7 +123,7 @@ public class SettingsActivity extends Activity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.settings_action_bar, menu);
+		//getMenuInflater().inflate(R.menu.settings_action_bar, menu);
 		return true;
 	}
 
